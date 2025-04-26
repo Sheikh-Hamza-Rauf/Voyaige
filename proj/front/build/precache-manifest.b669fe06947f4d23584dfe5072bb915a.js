d81ec96f361fb2922cf4e5db19c40883a31215a2
Sheikh-Hamza-Rauf <sh.hamzarauf@gmail.com>
2025-04-26 22:07:12 +0500

    Added gitignore and build command

---
+++ proj/front/build/precache-manifest.b669fe06947f4d23584dfe5072bb915a.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe096dddd2efedae33b7c76bc8399b8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939a7a83e015ae120578",
+    "url": "/static/css/main.7b98ff12.chunk.css"
+  },
+  {
+    "revision": "27b54c7cced3bc0bab48",
+    "url": "/static/js/2.69e25dbc.chunk.js"
+  },
+  {
+    "revision": "7ec40f8c3f656c2d27df",
+    "url": "/static/js/3.989c05ce.chunk.js"
+  },
+  {
+    "revision": "939a7a83e015ae120578",
+    "url": "/static/js/main.921f0447.chunk.js"
+  },
+  {
+    "revision": "c5059c2901e5566d26bf",
+    "url": "/static/js/runtime~main.32b44409.js"
+  },
+  {
+    "revision": "75f80922da2ba73f7d4b164c127ad2b8",
+    "url": "/static/media/LOGO.75f80922.png"
+  },
+  {
+    "revision": "f2e0b37eb3cd2746d4307261def4ae45",
+    "url": "/static/media/hotel.f2e0b37e.jpg"
+  },
+  {
+    "revision": "14facd18763de41d10a4c97e0c9a8914",
+    "url": "/static/media/hotel1.14facd18.jpg"
+  },
+  {
+    "revision": "1525f665fea1cb539783174633e672ab",
+    "url": "/static/media/hotel10.1525f665.jpg"
+  },
+  {
+    "revision": "3c7f750c3f7be0e7ec45ed931b80895b",
+    "url": "/static/media/hotel11.3c7f750c.jpeg"
+  },
+  {
+    "revision": "d9e9b817550c87ebdd93078d94f3cf9d",
+    "url": "/static/media/hotel12.d9e9b817.jpg"
+  },
+  {
+    "revision": "dc9d6a51481002c4e7ca46039acea4c8",
+    "url": "/static/media/hotel13.dc9d6a51.jpg"
+  },
+  {
+    "revision": "43ba4803738e459e3412c395060b3c89",
+    "url": "/static/media/hotel14.43ba4803.jpg"
+  },
+  {
+    "revision": "2a3c4e36f35efb81a9e07f912ead99b7",
+    "url": "/static/media/hotel15.2a3c4e36.jpeg"
+  },
+  {
+    "revision": "3ae4bc9fddc25f898856c7dc59fa2999",
+    "url": "/static/media/hotel2.3ae4bc9f.jpg"
+  },
+  {
+    "revision": "c404a5ddf734055f138307d7b9f8aa84",
+    "url": "/static/media/hotel3.c404a5dd.jpg"
+  },
+  {
+    "revision": "b38c7ead3f956977c9fb5aa673c6a1ec",
+    "url": "/static/media/hotel4.b38c7ead.jpg"
+  },
+  {
+    "revision": "47138b0de26bacdcb1877b97d7c5f769",
+    "url": "/static/media/hotel5.47138b0d.jpeg"
+  },
+  {
+    "revision": "f86d49b5396185031d748204a7889fa0",
+    "url": "/static/media/hotel6.f86d49b5.jpg"
+  },
+  {
+    "revision": "b30940e17c446bd1f9283e244ba8c33a",
+    "url": "/static/media/hotel7.b30940e1.jpg"
+  },
+  {
+    "revision": "a436c12023ae4b666566cc82a756cccd",
+    "url": "/static/media/hotel8.a436c120.jpeg"
+  },
+  {
+    "revision": "20c7df42d91956aaf90a571b6c1f6a7e",
+    "url": "/static/media/hotel9.20c7df42.jpg"
+  },
+  {
+    "revision": "fa12e237d92ba3a55428b8ca85aa9c37",
+    "url": "/static/media/restaurant.fa12e237.jpg"
+  },
+  {
+    "revision": "2a73633d818f9598049e418ba5e546d3",
+    "url": "/static/media/restaurant1.2a73633d.jpg"
+  },
+  {
+    "revision": "92c9ca2fe229e1365b1123ea1df8fbed",
+    "url": "/static/media/restaurant10.92c9ca2f.jpg"
+  },
+  {
+    "revision": "8ee77387b8b764669dd74a36fc4a7cc9",
+    "url": "/static/media/restaurant11.8ee77387.jpg"
+  },
+  {
+    "revision": "c75d26a6e62272c9805e20575902beb5",
+    "url": "/static/media/restaurant2.c75d26a6.jpg"
+  },
+  {
+    "revision": "cbd3f6219b8cf95c4d5fd71684e61060",
+    "url": "/static/media/restaurant3.cbd3f621.jpg"
+  },
+  {
+    "revision": "c3c147c93dd3c381cb0018a03a2fd6b7",
+    "url": "/static/media/restaurant4.c3c147c9.jpg"
+  },
+  {
+    "revision": "a1b5392df63563400973bfbc311a0ceb",
+    "url": "/static/media/restaurant5.a1b5392d.jpg"
+  },
+  {
+    "revision": "d1c474e8f651fe5824fb035addf08c6a",
+    "url": "/static/media/restaurant6.d1c474e8.jpg"
+  },
+  {
+    "revision": "7d0bcaae4e3e43be6cde7ac16fb83ed5",
+    "url": "/static/media/restaurant7.7d0bcaae.jpg"
+  },
+  {
+    "revision": "6f7351ca62d36dd9a58d765859342f87",
+    "url": "/static/media/restaurant8.6f7351ca.jpg"
+  },
+  {
+    "revision": "4a1675794dfe7d515fc8aedf3db5bf3d",
+    "url": "/static/media/restaurant9.4a167579.jpg"
+  },
+  {
+    "revision": "a266a76bf571a22f3124ebd5af11d4ca",
+    "url": "/static/media/transport.a266a76b.png"
+  }
+]);
