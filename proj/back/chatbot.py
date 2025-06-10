820e9bfe887c13835b4d83f784c31aacd29be125
harisalimughal <harisalimughal077@gmail.com>
2025-06-11 04:00:25 +0500

    Updated chatbot.py

--- proj/back/chatbot.py
+++ proj/back/chatbot.py
@@ -563,11 +563,3 @@ def milo():
 @app.route('/')
 def home():
     return "Welcome to the Flask App!"
-
-@app.route("/health")
-def health():
-    return "OK", 200
-    
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
