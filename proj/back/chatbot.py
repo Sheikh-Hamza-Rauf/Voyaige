5e62b7283620ddff1d22ceff23db9815223be4b2
harisalimughal <harisalimughal077@gmail.com>
2025-06-10 23:22:37 +0500

    Chatbot.py updated

--- proj/back/chatbot.py
+++ proj/back/chatbot.py
@@ -565,4 +565,5 @@ def home():
     return "Welcome to the Flask App!"
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
